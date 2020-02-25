6e9b398de3a24a6888fc4114213ec51a98241b70
Administrator <admin@example.com>
2020-02-25 14:27:43 +0800

    "2700000"

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -6,6 +6,7 @@ public class App
     private final String message = "Hello TAPD!";
     int a = 0;
     int b = 1;
+    String string = new String();
     public App() {}
 
     public static void main(String[] args) {
@@ -14,6 +15,7 @@ public class App
 
     private final String getMessage() {
     	int d = b/a;
+    	System.out.println(string.length());
     	System.out.println("this is my test");
     	System.out.println("this is my test");
         return message;
